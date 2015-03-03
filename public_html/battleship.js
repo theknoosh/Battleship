09008b2cb0bb5e366428f116d40594f232a4ff95
Darrell Payne <darrellpayne@earthlink.net>
2015-03-03 13:41:25 -0800

    Draws two tables and can click to change the color
    Only works in the right one at the moment

--- public_html/battleship.js
+++ public_html/battleship.js
@@ -1,11 +1,16 @@
-function generate_table() {
+function start(){
+    generate_table("tableOne");
+    generate_table("tableTwo");
+}
+
+function generate_table(myID) {
   // get the reference for the body
   var body = document.getElementsByTagName("body")[0];
  
   // creates a <table> element and a <tbody> element
   var tbl     = document.createElement("table");
   var tblBody = document.createElement("tbody");
-  tbl.setAttribute("id","myID");
+  tbl.setAttribute("id",myID);
   
   tbl.style.display = "inline-block";
   tbl.style.display = "float: left";
@@ -26,6 +31,7 @@ function generate_table() {
       a.appendChild(linkText);
       a.title = "test";
       var jLink = "javascript:change_cell("+i+","+j+");";
+      console.log(jLink);
       a.href = jLink;
       cell.appendChild(a);
       row.appendChild(cell);
@@ -45,5 +51,5 @@ function generate_table() {
 }
 
 function change_cell(row,col){
-   document.getElementById("myID").rows[row].cells[col].style.backgroundColor = "red"; 
+   document.getElementById("tableTwo").rows[row].cells[col].style.backgroundColor = "red"; 
 }
