a6c316a88488b3deaadedf7e517b3db08f040c3f
Darrell Payne <darrellpayne@earthlink.net>
2015-03-07 11:59:17 -0800

    Enemy tracking function working

--- public_html/battleship.js
+++ public_html/battleship.js
@@ -6,6 +6,8 @@ var computerBoard = [[0,0,0,1,0,0,0,1,0],[0,0,0,0,1,0,0,0,1],[1,0,0,0,1,0,0,0,0]
     [1,0,0,0,1,0,0,0,0],[0,0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0,0],[0,1,0,0,0,0,1,0,0],
     [1,0,0,0,1,0,0,0,0],[0,1,0,0,1,0,0,1,0]];
 
+var lastCell;
+
 function start(){
     generate_table("tableOne");
     generate_table("tableTwo");
@@ -84,8 +86,94 @@ function make_move(row,col){
     if(computerBoard[row][col]=== 1){
         document.getElementById("tableOne").rows[row].cells[col].innerHTML =
            "<img src = 'Images/water_red.jpg'>";
+//        hit_cell(row,col); // Register a hit
+        
     }else {
    document.getElementById("tableOne").rows[row].cells[col].innerHTML =
            "<img src = 'Images/water_grey.jpg'>";
     }
+    testFunction();
 }
+
+function hit_cell(row,col){
+    // Mark cell as hit
+    // Check to see if battleship sunk
+    // Check to see if ALL battleships sunk
+    // If all battleships sunk, player wins
+}
+
+function ComputerMoves() {
+    if(ComputerMoves.table === undefined){
+         ComputerMoves.table = document.getElementById('tableTwo');   
+    }
+    if(ComputerMoves.randomRow === undefined){
+        ComputerMoves.randomRow = Math.floor(Math.random() * 4);
+    }
+    if(ComputerMoves.randomCol === undefined){
+        ComputerMoves.randomCol = Math.floor(Math.random() * 4);
+    }
+    if(ComputerMoves.currentCol === undefined){
+        ComputerMoves.currentCol = 0;
+    }
+    if(ComputerMoves.currentRow === undefined){
+        ComputerMoves.currentRow = 0;
+    }
+    // Replace last cell if current cell is not 0
+    if(ComputerMoves.currentCol- 1 >=0){
+        ComputerMoves.table.rows[ComputerMoves.currentRow].cells[ComputerMoves.currentCol-1]
+            .innerHTML = ComputerMoves.lastCel;
+    }
+    
+    // Replace last cell in previous row
+    if(ComputerMoves.currentRow > 0 && ComputerMoves.currentCol === 0){
+        ComputerMoves.table.rows[ComputerMoves.currentRow-1].cells[8]
+            .innerHTML = ComputerMoves.lastCel;
+    }
+    
+    ComputerMoves.lastCel = ComputerMoves.table
+            .rows[ComputerMoves.currentRow].cells[ComputerMoves.currentCol].innerHTML;
+    console.log("Content in row:"+ComputerMoves.currentRow+" col:"+ComputerMoves.currentCol + " = "+ ComputerMoves.lastCel);
+    ComputerMoves.table.rows[ComputerMoves.currentRow].cells[ComputerMoves.currentCol]
+            .innerHTML = "<img src = 'Images/water_red.jpg'>";
+    
+    
+    
+    ComputerMoves.currentCol++;
+    if(ComputerMoves.currentCol > 8){
+        ComputerMoves.currentRow++;
+        if(ComputerMoves.currentRow > 8){
+            clearInterval(myVar);
+            // Reset all variables to undefined
+            ComputerMoves.currentCol = undefined;
+            ComputerMoves.currentRow = undefined;
+            ComputerMoves.randomCol = undefined;
+            ComputerMoves.randomRow = undefined;
+        }else{
+            ComputerMoves.currentCol = 0;
+        }
+    }
+}
+
+var myVar;
+
+function testFunction() {
+    myVar = setInterval(ComputerMoves, 100);
+}
+
+function alertFunc() {
+    alert("Hello!");
+    clearInterval(myVar);
+}
+
+//var startTime = new Date().getTime(),
+//    elapsed = '0.0',timeElapsed;
+
+//window.setInterval(function()
+//{
+//    timeElapsed = new Date().getTime() - startTime;
+//
+//    elapsed = Math.floor(timeElapsed / 100) / 10;
+//    if(Math.round(elapsed) == elapsed) { elapsed += '.0'; }
+////    console.log(elapsed);
+//
+//}, 100);
